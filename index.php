50d1921cf0cf9f80cf0911f5429631a61ad11028
marco <marcodecrema@libero.it>
2020-10-23 15:33:43 +0200

    foreach

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $utente1 = new User("Paolo","Rossi","45", "pr45","p.rossi@virgilio.it","pr0001#"
 $utente2 = new User("Mario","Bianchi","38", "mb38","m.bianchi@gmail.com","mb0002#");
 $utente3 = new User("Luca","Verdi","26", "lv26","l.verdi@outlook.com","lv0003#");
 
+$utenti = [$utente1, $utente2, $utente3];
 ?>
 
 <!DOCTYPE html>
@@ -42,10 +43,17 @@ $utente3 = new User("Luca","Verdi","26", "lv26","l.verdi@outlook.com","lv0003#")
       <th>MAIL</th>
       <th>PASSWORD</th>
 
-      <td><?php echo $utente1->$name ?></td>
-      <td><?php echo $utente1->$lastname ?></td>
-      <td><?php echo $utente1->$eta ?></td>
-      <td><?php echo $utente1->$username ?></td>
+      <?php foreach ($utenti as $utente) { ?>
+
+      <td><?php echo $utente->$name ?></td>
+      <td><?php echo $utente->$lastname ?></td>
+      <td><?php echo $utente>$eta ?></td>
+      <td><?php echo $utente->$username ?></td>
+      <td><?php echo $utente->$email ?></td>
+      <td><?php echo $utente->$password ?></td>
+
+    <?php } ?>
+
     </tr>
   </body>
 </html>
